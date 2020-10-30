666d39ab4e12147f29112c25638f7bb00b370ec6
Roman Shevtsov <Romses2016@yandex.ru>
2020-10-30 12:29:38 +0300

    task5

---
+++ src/task5_5/Main.java
@@ -0,0 +1,51 @@
+package task5_5;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int height = readHeight();
+
+        getTriangle(height);
+    }
+
+    public static int readHeight() {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the height of the triangle: ");
+        return scn.nextInt();
+    }
+
+    public static void getTriangle(int height) {
+        for (int i = 0; i < height; i++) {
+            printTopTriangle(i);
+            printBottomTriangle(i, height);
+            printMiddleTriangle(i, height);
+        }
+    }
+
+    public static void printTopTriangle(int i) {
+        if (i == 0) {
+            System.out.println("*");
+        }
+    }
+
+    public static void printMiddleTriangle(int i, int height) {
+        if (i > 0 && i < height - 1) {
+            System.out.print("|");
+            for (int j = 1; j < i; i--) {
+                System.out.print(" ");
+            }
+            System.out.println("\\");
+        }
+    }
+
+    public static void printBottomTriangle(int i, int height) {
+        if (i == height - 1) {
+            for (int c = 0; c < height; c++) {
+                System.out.print((c == 0 || c == height - 1) ? '*' : '-');
+            }
+            System.out.println();
+        }
+    }
+}
